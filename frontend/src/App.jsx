b7e6bfc4b423f3ead6bee6145f93b2f8825c7bec
YashPatel1912 <yash01912@gmail.com>
2026-08-14 08:10:23 +0530

    update fast api url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import "./style.css";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://mental-health-score-predictor-ml-iell.onrender.com";
 
 const initialForm = {
   age: "",
